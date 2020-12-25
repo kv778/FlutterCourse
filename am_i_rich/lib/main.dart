e3b1f7c9a71f25721d3b1bb999d3e0dbc4570eba
Kartik <kartikvishnoi778@gmail.com>
2020-12-26 01:01:15 +0530

    Final version of I Am Rich

--- am_i_rich/lib/main.dart
+++ am_i_rich/lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
   MaterialApp(
-    home: Center(
-      child: Text('Hello World!'),
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      backgroundColor: Colors.blueGrey[300],
+      body: Center(
+        child: Image(
+          image: AssetImage('images/space.jpg'),
+        ),
+      ),
     ),
   ),
 );
