674c163cb100b0113c93dc2510a9e18040714121
Kartik <kartikvishnoi778@gmail.com>
2020-12-26 01:08:20 +0530

    Created i_am_poor and added its AppBar

---
+++ i_am_poor/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I Am Poor :('),
+        backgroundColor: Colors.blueGrey[600],
+      ),
+      backgroundColor: Colors.blueGrey[200],
+    ),
+  ),
+);
