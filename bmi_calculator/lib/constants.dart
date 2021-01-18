3616e64dde46087406ff861e20b54476588ddf9f
Kartik <kartikvishnoi778@gmail.com>
2021-01-19 01:25:57 +0530

    Added UI for the Results page

--- bmi_calculator/lib/constants.dart
+++ bmi_calculator/lib/constants.dart
@@ -5,6 +5,9 @@ const kInactiveCardColor = Color(0xFF111328);
 const kActiveCardColor = Color(0xFF1D1E33);
 const kBottomContainerColor = Color(0xFFEB1555);
 
+const int minHeight = 120;
+const int maxHeight = 220;
+
 const TextStyle kLabelTextStyle = TextStyle(
   fontSize: 25.0,
   color: Color(0xFF8D8E98),
@@ -16,5 +19,21 @@ const TextStyle kHeightText = TextStyle(
   fontWeight: FontWeight.w900,
 );
 
-const int minHeight = 120;
-const int maxHeight = 220;
+const kTitleTextStyle = TextStyle(
+  fontSize: 45.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  fontSize: 30.0,
+    color: Color(0xFF24D876),
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
--- bmi_calculator/lib/results_page.dart
+++ bmi_calculator/lib/results_page.dart
@@ -1,4 +1,7 @@
+import 'package:bmi_calculator/reusable_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -8,7 +11,75 @@ class ResultsPage extends StatelessWidget {
         title: Text('Results Page'),
       ),
       body: Container(
-        child: Text('Hello there, General Kenobi'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Your Result',
+                  style: kTitleTextStyle,
+                ),
+              ),
+            ),
+
+            Expanded(
+              flex: 5,
+              child: ReusableCard(
+                colour: kActiveCardColor,
+                cardChild: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Normal',
+                        style: kResultTextStyle,
+                      ),
+                      Text(
+                        '6.9',
+                        style: kBMITextStyle,
+                      ),
+                      Text(
+                        'Your BMI is quite low, you need to eat more',
+                        style: kBodyTextStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 15.0),
+                  color: kBottomContainerColor,
+                  height: kBottomContainerHeight,
+                  width: double.infinity,
+                  child: Center(
+                    child: Text(
+                      'RE-CALCULATE',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0,
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
