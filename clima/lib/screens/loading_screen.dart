23758aab53385b0ad56bb950ad434ae821b78b5e
Kartik <kartikvishnoi778@gmail.com>
2021-02-18 02:23:22 +0530

    Implemented fetching weather data with API key

--- clima/lib/screens/loading_screen.dart
+++ clima/lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'location_screen.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,9 +23,19 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(obj.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get('https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=72e25a52b669758e337b66aa0068dd27');
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
     );
   }
 }
+
+//account name: hsi35902
+//password: giveapikey
+//key: 72e25a52b669758e337b66aa0068dd27
