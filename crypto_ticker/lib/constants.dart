d85358ab8c3b45fc008d064566f011b9cca2a11c
Kartik <kartikvishnoi778@gmail.com>
2021-02-23 03:00:12 +0530

    Added some data to try out a different API

--- crypto_ticker/lib/constants.dart
+++ crypto_ticker/lib/constants.dart
@@ -1,3 +1,1 @@
-String apiKey = '47CD1431-413A-4CC9-8638-087224A0EC24';
-
-String apiKey2 = 'gerkajabh8nzpa68gpuu5i';
+String apiKey = '8b347efa4c1abd06ade94d54ff27afc36d231958b17805d2f26565b70be36293';
--- crypto_ticker/lib/networking.dart
+++ crypto_ticker/lib/networking.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'constants.dart';
 
 class NetworkHelper {
   NetworkHelper({this.currency});
@@ -7,6 +8,7 @@ class NetworkHelper {
   var price;
   Future<dynamic> getPrice(String crypto) async {
     http.Response response = await http.get('https://api.coincap.io/v2/assets/$crypto');
+    //http.Response response = await http.get('https://min-api.cryptocompare.com/data/price?fsym=$crypto&tsyms=USD&api_key=$apiKey');
     String data = response.body;
     price =  jsonDecode(data)['data'][currency];
     crypto = '';
